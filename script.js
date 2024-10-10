68b1b124f24cc484d659fed185afc1cf52af707f
Danna98 <70389592+Danna98@users.noreply.github.com>
2024-10-10 10:56:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-mermaid.initialize({
-    startOnLoad: true,
-    theme: 'default',
-    flowchart: {
-        useMaxWidth: false,
-        htmlLabels: true,
-        curve: 'basis'
-    },
-    themeVariables: {
-        primaryColor: '#9b59b6',
-        primaryTextColor: '#fff',
-        primaryBorderColor: '#8e44ad',
-        lineColor: '#8e44ad',
-        secondaryColor: '#ff0000',
-        tertiaryColor: '#fff'
-    }
+document.addEventListener('DOMContentLoaded', (event) => {
+    const sections = document.querySelectorAll('.section');
+    
+    sections.forEach(section => {
+        section.addEventListener('click', () => {
+            section.classList.toggle('active');
+        });
+    });
 });
